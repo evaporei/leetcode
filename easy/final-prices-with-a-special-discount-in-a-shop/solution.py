b770a9714ed72173545a3dd8a66a9f6739abaf3a
Eva Pace <eba.pachi@gmail.com>
2024-05-02 08:48:32 -0300

    easy: make final prices w/ discount more pythonic

--- easy/final-prices-with-a-special-discount-in-a-shop/solution.py
+++ easy/final-prices-with-a-special-discount-in-a-shop/solution.py
@@ -2,10 +2,8 @@ class Solution:
     def finalPrices(self, prices: list[int]) -> list[int]:
         finals = prices.copy()
         for i, price in enumerate(prices):
-            j = i + 1
-            while j < len(prices):
-                if prices[j] <= price:
-                    finals[i] -= prices[j]
+            for discount in prices[i+1:]:
+                if discount <= price:
+                    finals[i] -= discount
                     break
-                j += 1
         return finals
