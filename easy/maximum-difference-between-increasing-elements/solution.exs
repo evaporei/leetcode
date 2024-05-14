6423313b77280522f0f9d08e94a0b7b3718ec466
Eva Pace <eba.pachi@gmail.com>
2024-05-14 01:03:59 -0300

    easy: better elixir maximum difference between increasing elements

--- easy/maximum-difference-between-increasing-elements/solution.exs
+++ easy/maximum-difference-between-increasing-elements/solution.exs
@@ -1,12 +1,12 @@
 defmodule Solution do
   @spec maximum_difference(nums :: [integer]) :: integer
   def maximum_difference(nums) do
-    [r, m] = Enum.reduce(nums, [-1, 2305843009213693951], fn curr, [res, min_so_far] ->
+    {r, m} = Enum.reduce(nums, {-1, :inf}, fn curr, {res, min_so_far} ->
       min_so_far = min(min_so_far, curr)
       if curr > min_so_far do
-        [max(res, curr - min_so_far), min_so_far]
+        {max(res, curr - min_so_far), min_so_far}
       else
-        [res, min_so_far]
+        {res, min_so_far}
       end
     end)
     r
