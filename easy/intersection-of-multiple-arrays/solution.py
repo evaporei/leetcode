a60531ac197467497f8f556223264f3f32c5e497
Eva Pace <eba.pachi@gmail.com>
2024-05-28 19:36:35 -0300

    easy/python: intersection of multiple arrays

---
+++ easy/intersection-of-multiple-arrays/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def intersection(self, nums: List[List[int]]) -> List[int]:
+        res = set(nums[0])
+        for i in range(1, len(nums)):
+            res &= set(nums[i])
+        res = list(res)
+        res.sort()
+        return res
