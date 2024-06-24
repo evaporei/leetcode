8b7f94113452bad95df44dfd71bd4766a2fc4c89
Eva Pace <eba.pachi@gmail.com>
2024-06-24 16:01:57 -0300

    hard/python: binary tree maximum path sum

---
+++ hard/binary-tree-maximum-path-sum/solution.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        res = root.val
+
+        def dfs(node: Optional[TreeNode]) -> int:
+            if not node:
+                return 0
+            left_max = dfs(node.left)
+            right_max = dfs(node.right)
+
+            left_max = max(left_max, 0)
+            right_max = max(right_max, 0)
+
+            nonlocal res
+            res = max(res, node.val + left_max + right_max)
+
+            return node.val + max(left_max, right_max)
+            
+        dfs(root)
+        return res
