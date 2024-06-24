dda2df9f71b8d11f3a2b4c2dc87cb6274689a47d
Eva Pace <eba.pachi@gmail.com>
2024-06-24 12:12:08 -0300

    medium/python: count good nodes in binary tree

---
+++ medium/count-good-nodes-in-binary-tree/true_pure.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        def dfs(node: Optional[TreeNode], max_val: int) -> int:
+            if not node:
+                return 0
+            res = 1 if node.val >= max_val else 0
+            new_max = max(max_val, node.val)
+            return res + dfs(node.left, new_max) + dfs(node.right, new_max)
+
+        return dfs(root, root.val)
