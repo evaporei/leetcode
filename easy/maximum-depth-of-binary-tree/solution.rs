2e3ebf51d26ce438acc09fe06e832b3f535eedee
Eva Pace <eba.pachi@gmail.com>
2025-04-28 19:24:24 -0300

    easy/rust: maximum depth of binary tree

---
+++ easy/maximum-depth-of-binary-tree/solution.rs
@@ -0,0 +1,33 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        match root {
+            Some(r) => {
+                1 + std::cmp::max(
+                    Solution::max_depth(r.borrow().left.clone()),
+                    Solution::max_depth(r.borrow().right.clone()),
+                )
+            }
+            None => 0,
+        }
+    }
+}
